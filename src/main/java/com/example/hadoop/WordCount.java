de41562c2aa580d14bb8874ab13d535ec769553a
Wei Yan <74025253+thhreh@users.noreply.github.com>
2024-11-13 23:37:36 -0800

    Update WordCount.java

--- src/main/java/com/example/hadoop/WordCount.java
+++ src/main/java/com/example/hadoop/WordCount.java
@@ -7,10 +7,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache
 
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -23,9 +20,7 @@ public class WordCount {
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
+        ) throws IOExTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
             }
@@ -49,10 +44,7 @@ public class WordCount {
     }
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
+        Configuration conizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
